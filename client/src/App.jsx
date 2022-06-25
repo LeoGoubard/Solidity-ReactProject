acd1a9027a2c3353f82e82f8f659ab99825ca0eb
LEO GOUBARD <goubardleo@gmail.com>
2022-06-25 16:54:25 +0200

    first commit'

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello World
+      </h1>
+    </div>
+  )
+}
+
+export default App
