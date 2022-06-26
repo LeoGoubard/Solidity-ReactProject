807b6097bcadaad73ecd007a9172d8caa196460e
LEO GOUBARD <goubardleo@gmail.com>
2022-06-26 20:43:30 +0200

    :construction: Welcome done, wip blockchain/smartcontract wip

--- client/src/Components/Welcome/index.jsx
+++ client/src/Components/Welcome/index.jsx
@@ -6,72 +6,98 @@ import { BsInfoCircle } from "react-icons/bs";
 import { Loader } from "../";
 
 const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
-
-const Input = ({ placeholder, name, type, value, handleChange }) => (
-  <input
-    placeholder={placeholder}
-    type={type}
-    step="0.0001"
-    value={value}
-    onChange={(e) => handleChange(e, name)}
-    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
-  />
-);
+const differentInputs = [
+  {
+    placeholder: 'Address To',
+    name: 'addressTo',
+    type: 'text',
+  },
+  {
+    placeholder: 'Amount (ETH)',
+    name: 'amount',
+    type: 'number',
+  },
+  {
+    placeholder: 'Keyword (Gif)',
+    name: 'keyword',
+    type: 'text',
+  },
+  {
+    placeholder: 'Enter Message',
+    name: 'message',
+    type: 'text',
+  }
+]
+const Input = ({ placeholder, name, type, value, handleChange }) => {
+  console.log(placeholder);
+  return (
+    <input
+      placeholder={placeholder}
+      type={type}
+      step="0.0001"
+      value={value}
+      onChange={(e) => handleChange(e, name)}
+      className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    />
+  )
+};
 
 const Welcome = () => {
 
   const handleSubmit = (e) => {
     const { addressTo, amount, keyword, message } = formData;
-
+  
     e.preventDefault();
 
     if (!addressTo || !amount || !keyword || !message) return;
 
     sendTransaction();
   };
-
+  const connectWallet = () => {
+    console.log('hello there');
+  }
   return (
     <div className="flex w-full justify-center items-center">
-      <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-        <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
-          <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
-            Send Crypto <br /> across the world
-          </h1>
-          <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
-            Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
-          </p>
-          {/* {!currentAccount && (
-            <button
-              type="button"
-              onClick={connectWallet}
-              className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
-            >
-              <AiFillPlayCircle className="text-white mr-2" />
-              <p className="text-white text-base font-semibold">
-                Connect Wallet
-              </p>
-            </button>
-          )} */}
+    <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+      <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
+        <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
+          Send Crypto <br /> across the world
+        </h1>
+        <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
+          Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
+        </p>
+        {!true && (
+          <button
+            type="button"
+            onClick={connectWallet}
+            className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+          >
+            <AiFillPlayCircle className="text-white mr-2" />
+            <p className="text-white text-base font-semibold">
+              Connect Wallet
+            </p>
+          </button>
+        )}
 
-          <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
-            <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
-              Reliability
-            </div>
-            <div className={companyCommonStyles}>Security</div>
-            <div className={`sm:rounded-tr-2xl ${companyCommonStyles}`}>
-              Ethereum
-            </div>
-            <div className={`sm:rounded-bl-2xl ${companyCommonStyles}`}>
-              Web 3.0
-            </div>
-            <div className={companyCommonStyles}>Low Fees</div>
-            <div className={`rounded-br-2xl ${companyCommonStyles}`}>
-              Blockchain
-            </div>
+        <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
+          <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
+            Reliability
+          </div>
+          <div className={companyCommonStyles}>Security</div>
+          <div className={`sm:rounded-tr-2xl ${companyCommonStyles}`}>
+            Ethereum
+          </div>
+          <div className={`sm:rounded-bl-2xl ${companyCommonStyles}`}>
+            Web 3.0
+          </div>
+          <div className={companyCommonStyles}>Low Fees</div>
+          <div className={`rounded-br-2xl ${companyCommonStyles}`}>
+            Blockchain
           </div>
         </div>
+      </div>
 
-        <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
+      <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
           <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
             <div className="flex justify-between flex-col w-full h-full">
               <div className="flex justify-between items-start">
@@ -82,7 +108,7 @@ const Welcome = () => {
               </div>
               <div>
                 <p className="text-white font-light text-sm">
-                  {/*shortenAddress(currentAccount)*/}
+                  {/* shortenAddress(currentAccount) */}
                 </p>
                 <p className="text-white font-semibold text-lg mt-1">
                   Ethereum
@@ -91,14 +117,14 @@ const Welcome = () => {
             </div>
           </div>
           <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-            <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => handleChange} />
-            <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => handleChange} />
-            <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => handleChange} />
-            <Input placeholder="Enter Message" name="message" type="text" handleChange={() => handleChange} />
-
+            {differentInputs.map((inputItem) => {
+              const { placeholder, name, type } = inputItem;
+            return (
+              <Input placeholder={placeholder} name={name} type={type} handleChange={() => handleChange} />
+            )})}
             <div className="h-[1px] w-full bg-gray-400 my-2" />
 
-            {/* isLoading
+            {true
               ? <Loader />
               : (
                 <button
@@ -108,7 +134,7 @@ const Welcome = () => {
                 >
                   Send now
                 </button>
-              ) */}
+              )}
           </div>
         </div>
       </div>
