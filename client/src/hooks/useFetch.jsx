4df31e02a109ff55850692da6449d6148e651326
LEO GOUBARD <goubardleo@gmail.com>
2022-06-27 14:03:40 +0200

    :zap: remove clg

--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -9,7 +9,6 @@ const useFetch = ({ keyword }) => {
       const { data } = await response.json();
       setGifUrl(data[0]?.images?.downsized_medium.url);
     } catch (error) {
-      console.log(error);
       setGifUrl("https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284");
     }
   };
